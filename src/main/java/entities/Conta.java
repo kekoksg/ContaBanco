1ea080f31225e6864636e396256d7d194ae02925
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2024-07-28 20:24:36 -0300

    Add Conta

--- src/main/java/entities/Conta.java
+++ src/main/java/entities/Conta.java
@@ -1,4 +1,52 @@
 package entities;
 
 public class Conta {
+
+    private String nome;
+    private String agencia;
+    private int numero;
+    private double saldo;
+
+    public Conta(String nome, String agencia, int numero, double saldo) {
+        this.nome = nome;
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public Conta() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
 }

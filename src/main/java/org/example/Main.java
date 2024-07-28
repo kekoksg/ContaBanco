ca852f9a9af15e0a4c47dd64d6653ee71ef647e7
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2024-07-28 20:25:38 -0300

    Projeto finalizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import entities.Conta;
+import service.ContaTerminal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContaTerminal contaTerminal = new ContaTerminal();
+        Conta conta = contaTerminal.coletarInformacoes();
+        contaTerminal.mostrarInformacoes(conta);
     }
 }
